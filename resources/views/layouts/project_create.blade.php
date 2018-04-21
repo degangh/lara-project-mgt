1f714d25433302c29f8a97d2be7e1c18d06ea3f1
nicolas <degang.huang@gmail.com>
2018-04-21 22:54:04 +0930

    hide create form onload using jqueryui's dialog

--- resources/views/layouts/project_create.blade.php
+++ resources/views/layouts/project_create.blade.php
@@ -1,25 +1,52 @@
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-            <div class="panel-heading">Add Project</div>
+<div class="create-project" title="Create a New Project">
+    
+            
             <form action="{{ url('/projects') }}" method="post">
             {{csrf_field()}}
-            <div class="panel-body">
+            
                 <div class="form-group">
-                    <lable for="project-name" class="control-label">Project</label>
+                    <lable for="project-name" class="control-label">Project Name</label>
 
                     <div class="">
                         <input type="text" name = "name" id="project-name" class="form-control">
                     </div>
                 </div>
+
+                <div class="form-group">
+                    <lable for="project-name" class="control-label">Project Name</label>
+
+                    <div class="">
+                        <textarea name = "desc" class="form-control" rows="3"></textarea>
+                    </div>
+                </div>
+
                 <div class="form-group">
                     <div class="">
                         <button class="btn btn-default">
-                        <i class="glyphicon glyphicon-plus-sign"></i> Add New Project
+                        <i class="glyphicon glyphicon-plus-sign"></i> Save
                         </button>
                     </div>
                 </div>
-            </div>
-        </div>
+        
+  
+</div>
+<div class="row" style="margin-bottom:15px">
+    <div class="col-md-8 col-md-offset-2">
+        <button class="btn btn-default create-project-btn btn-success pull-right" >
+        <i class="glyphicon glyphicon-plus-sign"></i> Add New Project
+        </button>
     </div>
-    </div>
+</div>
+
+<script>
+jQuery(function(){
+    jQuery(".create-project").dialog({
+        autoOpen: false,
+        width: "500px"
+    });
+
+    jQuery(".create-project-btn").on("click", function(){
+        jQuery(".create-project").dialog("open")
+    })
+})
+</script>
