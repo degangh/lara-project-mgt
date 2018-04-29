0b38c3f143871099c2ae384992afdeb9136b781c
nicolas <degang.huang@gmail.com>
2018-04-29 21:26:44 +0930

    create new table project_user

---
+++ database/migrations/2018_04_29_114711_create_project_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_user', function (Blueprint $table) {
+            $table->integer('project_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_user');
+    }
+}
