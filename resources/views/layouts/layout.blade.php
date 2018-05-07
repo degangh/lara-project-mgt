388a13bcb29a4e02fae76e3fb560dc54ed8802a1
degangh <degang.huang@gmail.com>
2018-05-07 11:48:11 +0930

    remove cdn from layout files

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -29,8 +29,7 @@
     <script src="{{ mix('js/jquery-ui.min.js') }}"></script>
     <script src="{{ mix('js/select2/select2.min.js') }}"></script>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
     
 </head>
 
