9ec622cd6f989277ee224598f2dcaf4c3c24ec0d
degangh <degang.huang@gmail.com>
2018-05-10 10:02:28 +0930

    refine cards layout in member list

--- resources/views/layouts/member_list.blade.php
+++ resources/views/layouts/member_list.blade.php
@@ -1,19 +1,18 @@
-@foreach ($project->members as $member)
-    {{$member->name}}
-@endforeach
 
 <div class="row">
         <div class="offset-md-1 col-md-10">
             <div class="card">
             <div class="card-header">Members in {{$project->name}}</div>
                 <div class="card-body">
+                <div class="row">
                     @foreach ($project->members as $member)
-                    <div class="card col-md-3" >
+                    <div class="card col-md-2 m-1" >
                     <div class="card-body">
                     {{$member->name}}
                     </div>
                     </div>
                     @endforeach
+                </div>
                 </div>
             
             </div>
