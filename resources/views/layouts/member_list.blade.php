471b393b73c1992d16e24458c28331cbae4ee31e
nicolas <degang.huang@gmail.com>
2018-05-12 09:12:58 +0930

    add project owner to the member list and mark it as with a star

--- resources/views/layouts/member_list.blade.php
+++ resources/views/layouts/member_list.blade.php
@@ -3,10 +3,16 @@
         <div class="offset-md-1 col-md-10">
             <div class="card">
             <div class="card-header">Members in {{$project->name}}</div>
-                <div class="card-body">
+                <div class="card-body text-nowrap">
                 <div class="row">
+                <div class="card col-md-2 m-1 p-1" >
+                    <div class="card-body">
+                    {{$project->user->name}} 
+                    </div>
+                    <span class="fa fa-star text-warning" style="position:absolute;top:-7px;right:-7px;font-size:25px"></span>
+                    </div>
                     @foreach ($project->members as $member)
-                    <div class="card col-md-2 m-1" >
+                    <div class="card col-md-2 m-1 p-1" >
                     <div class="card-body">
                     {{$member->name}}
                     </div>
