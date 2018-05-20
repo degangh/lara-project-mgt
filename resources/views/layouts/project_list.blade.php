bda9e3fa96d56faf5817a5a2de34d7de77db05b0
nicolas <degang.huang@gmail.com>
2018-05-20 11:19:16 +0930

    differntiate non-owner projects

--- resources/views/layouts/project_list.blade.php
+++ resources/views/layouts/project_list.blade.php
@@ -8,12 +8,19 @@
                 <tr>
                 <td>
                 {{$project->name}}
+                @if ($project->user->id != Auth::user()->id)
+                <span class="fa fa-users"></span>
+                @endif
                 </td>
                 <td>
                 <a href="{{url('/projects')}}/{{$project->id}}" ><i class="fa fa-folder-open"></i></a>
                 </td>
                 <td>
+                @if ($project->user->id == Auth::user()->id)
                 <a href="#" class="edit-project-btn" data-project-name="{{$project->name}}" data-desc="{{$project->desc}}" data-id = "{{$project->id}}"><i class="fa fa-edit"></i></a>
+                @else
+                <span class="fa fa-lock"></span>
+                @endif
                 </td>
                 </tr>
                 @endforeach
