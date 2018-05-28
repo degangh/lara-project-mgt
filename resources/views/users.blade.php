f64778a87cc89b5aa59d6945991879174da4b424
nicolas <degang.huang@gmail.com>
2018-05-28 22:01:03 +0930

    change the layout files

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.layout')
 
 @section('content')
 @include('layouts.message')
+@include('layouts.user.create')
 @include('layouts.user.list')
 
 @endsection
