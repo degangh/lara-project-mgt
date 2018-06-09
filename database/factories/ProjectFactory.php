4bc1958a151897abb74aa75b7f1c52e01aae6593
nicolas <degang.huang@gmail.com>
2018-06-09 21:21:33 +0930

    refine task and project factories

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -3,14 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Project::class, function (Faker $faker) {
+        
     return [
         //
         "name" => $faker->title,
-        "owner->id" => function(){
+        "owner_id" => function(){
             return factory(App\User::class)->create()->id;
         },
-        "created_at" => $now,
-        "updated_at" => $now,
         "desc" => $faker->sentence
     ];
 });
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
-$factory->define(Model::class, function (Faker $faker) {
+
+$factory->define(App\Task::class, function (Faker $faker) {
     return [
-        //
+        "project_id" => function(){},
+        "name" => $faker->title,
+        "user_id" => function(){},
+        "due_time" => $faker->dateTimeBetween("now", "+2 years")->format("Y-m-d")
+        
     ];
 });
