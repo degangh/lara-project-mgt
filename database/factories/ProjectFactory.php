487a08d353afb598cde34101ac7e94350c85d455
nicolas <degang.huang@gmail.com>
2018-06-11 21:18:17 +0930

    replace title with realtext

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Project::class, function (Faker $faker) {
         
     return [
         //
-        "name" => $faker->title,
+        "name" => $faker->realText(50),
         "owner_id" => function(){
             return factory(App\User::class)->create()->id;
         },
