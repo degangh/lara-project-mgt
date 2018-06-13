5b674303541b2c5312f120214614bd4960476488
nicolas <degang.huang@gmail.com>
2018-06-13 21:38:20 +0930

    task creation test

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,19 +2,45 @@
 
 namespace Tests\Feature;
 
+
+use App\Project;
+use App\Task;
+use App\User;
+use \Crypt;
+use Session;
+
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    /*
+
+    */
+    public function testTaskCreation()
     {
-        $this->assertTrue(true);
+        $user = User::first();
+        $project = $user->projects()->first();
+        $task = factory(Task::class)->make();
+        var_dump($task);
+        $response = $this->actingAs($user)->post(route('tasks.store'), [
+            'project_id' => Crypt::encrypt($project->id),
+            'due_date' => $task->due_time,
+            'name' => $task->name,
+            'user_id' => $user->id
+        ]);
+        var_dump(Session::get('errors'));
+        $response->assertStatus(302);
+        
+        $response->assertSessionHas('success', 'Task Saved Successfully');
+
+    }
+
+    /*
+    */
+    public function testTaskComplete()
+    {
+
     }
 }
