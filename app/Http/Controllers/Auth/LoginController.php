a8fdef3afffe04a75818ea2f901a4fa3e1f43bf4
degangh <degang.huang@gmail.com>
2018-08-08 09:59:03 +0930

    customize longinController, add one more column to validate login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
     /*
@@ -35,5 +37,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * override credential
+     */
+    protected function credentials(Request $request)
+    {
+        $request['is_active'] = 1;
+        return $request->only($this->username(), 'password', 'is_active');
     }
 }
