ca83930ca0ba3ac194684f5d626fe521c06d41f5
nicolas <degang.huang@gmail.com>
2018-08-12 22:53:54 +0930

    Add: only admin user is able to list all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     
     public function __construct()
     {
-        $this->middleware('auth');   
+        $this->middleware('auth');
     }
     
     /**
@@ -21,7 +21,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $this->authorize("edit", User::class);   
+        
         $users = User::all();
 
         return view('users', [
