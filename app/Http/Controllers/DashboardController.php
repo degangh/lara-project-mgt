bf6faae05c6ebf50a45707369a1d040f784c5ff4
degangh <degang.huang@gmail.com>
2018-08-17 17:30:08 +0930

    Plan dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Task;
+use App\Project;
 
 class DashboardController extends Controller
 {
@@ -10,5 +13,16 @@ class DashboardController extends Controller
     public function index()
     {
         echo 'dashboard'; //to do
+
+        $projectCount = Project::count();
+
+        echo $projectCount;
+
+        $userCount = User::count();
+
+        $taskCount = Task::count();
+
+        echo ":" . $userCount . ":" . $taskCount;
+        
     }
 }
