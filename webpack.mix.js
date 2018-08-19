c54c9a12f9a4e36b5cc4f31401c309082ed5afb7
degangh <degang.huang@gmail.com>
2018-08-20 09:00:34 +0930

    rebuild frontend assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,13 +16,13 @@ mix.js('resources/assets/js/app.js', 'public/js')
    mix.copy('node_modules/jquery-ui-dist/jquery-ui.min.js', 'public/js');
    mix.copy('node_modules/jquery-ui-dist/jquery-ui.min.css', 'public/css');
    mix.copy('node_modules/jquery-ui-dist/images', 'public/css/images');
-   /*
+   
    mix.copy('node_modules/startbootstrap-sb-admin-2/dist/css', 'public/css/sbadmin2');
    mix.js('node_modules/startbootstrap-sb-admin-2/dist/js/sb-admin-2.min.js', 'public/js/sbadmin2');
    mix.copy('node_modules/metismenu/dist/metisMenu.min.css', 'public/css/metismenu');
    mix.copy('node_modules/metismenu/dist/metisMenu.min.css.map', 'public/css/metismenu');
    mix.copy('node_modules/metismenu/dist/metisMenu.min.js', 'public/js/metismenu');
    mix.copy('node_modules/metismenu/dist/metisMenu.min.js.map', 'public/js/metismenu');
-   */
+   
    mix.copy('node_modules/select2/dist/js/select2.min.js', 'public/js/select2');
    mix.copy('node_modules/select2/dist/css/select2.min.css', 'public/css/select2');
