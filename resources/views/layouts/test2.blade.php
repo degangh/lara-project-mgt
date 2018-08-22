b3b36af4669d0facf324086b8250b0bef2aaf21f
degangh <degang.huang@gmail.com>
2018-08-22 11:19:01 +0930

    recompile frontend assets, update to fontawesome to 5.2

--- resources/views/layouts/test2.blade.php
+++ resources/views/layouts/test2.blade.php
@@ -14,7 +14,7 @@
 
    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <!-- Custom fonts for this template-->
-    <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    
 
     <!-- Page level plugin CSS-->
     <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
