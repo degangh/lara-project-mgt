5aa72d7f0b6bee030ec73edaa94605f331d30ef7
degangh <degang.huang@gmail.com>
2018-08-23 13:04:12 +0930

    Apply new theme to testing template

--- resources/views/layouts/test2.blade.php
+++ resources/views/layouts/test2.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>SB Admin - Blank Page</title>
+    <title>{{ config('app.name', 'Project') }}</title>
 
    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <!-- Custom fonts for this template-->
@@ -33,26 +33,15 @@
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-      <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
+      <a class="navbar-brand mr-1" href="index.html">Project</a>
 
       <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
       </button>
 
-      <!-- Navbar Search -->
-      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-          <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
-              <i class="fas fa-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
 
       <!-- Navbar -->
-      <ul class="navbar-nav ml-auto ml-md-0">
+      <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown no-arrow mx-1">
           <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-bell fa-fw"></i>
