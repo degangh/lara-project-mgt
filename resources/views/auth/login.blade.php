aba891cf2503451101695f6e64664c273a07b4a5
degangh <degang.huang@gmail.com>
2018-08-23 13:03:50 +0930

    Apply new theme to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,80 @@
-@extends('layouts.layout')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+  <head>
 
-<div class="row p-3">
-<div class="card offset-md-3 col-md-6 p-0">
-    <div class="card-header">Login</div>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <div class="card-body">
-        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-            {{ csrf_field() }}
+    <title>{{config('app.name', 'Project')}} - Login</title>
 
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label for="email">E-Mail Address</label>
+    <!-- Bootstrap core CSS-->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 
+    
+    <!-- Custom styles for this template-->
+    <link href="{{ mix('/css/sbadmin/sb-admin.css') }}" rel="stylesheet">
 
-                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+  </head>
 
-                    @if ($errors->has('email'))
+  <body class="bg-dark">
+
+    <div class="container">
+      <div class="card card-login mx-auto mt-5">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+          <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+          {{ csrf_field() }}
+            <div class="form-group">
+              <div class="form-label-group">
+                <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required="required" autofocus="autofocus" value = "{{ old('email') }}">
+                @if ($errors->has('email'))
                         <span class="help-block">
                             <strong>{{ $errors->first('email') }}</strong>
                         </span>
-                    @endif
-
+                @endif
+                <label for="inputEmail">Email address</label>
+              </div>
             </div>
-
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label for="password" >Password</label>
-
-
-                    <input id="password" type="password" class="form-control" name="password" required>
-
-                    @if ($errors->has('password'))
+            <div class="form-group">
+              <div class="form-label-group">
+                <input type="password" id="password" class="form-control" placeholder="Password" required="required" name = 'password'>
+                @if ($errors->has('password'))
                         <span class="help-block">
                             <strong>{{ $errors->first('password') }}</strong>
                         </span>
                     @endif
-
+                <label for="inputPassword">Password</label>
+              </div>
             </div>
-
             <div class="form-group">
-
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                        </label>
-                    </div>
-
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="remember-me">
+                  Remember Password
+                </label>
+              </div>
             </div>
-
-            <div class="form-group">
-
-                    <button type="submit" class="btn btn-primary">
-                        Login
-                    </button>
-
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        Forgot Your Password?
-                    </a>
-
-            </div>
-        </form>
+            <input type='submit' class="btn btn-primary btn-block" value="Login">
+          </form>
+          <div class="text-center">
+            <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-</div>
 
-@endsection
+        <!-- Bootstrap core JavaScript-->
+
+    <script src="{{ mix('js/app.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ mix('js/sbadmin/sb-admin.min.js') }}"></script>
+    <script src="{{ mix('js/jquery.easing.js') }}"></script>
+
+  </body>
+
+</html>
