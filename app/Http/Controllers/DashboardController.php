e12aebc0e6a9c8b1c10b43eafa869df760e09cea
nicolas <degang.huang@gmail.com>
2018-08-25 21:43:13 +0930

    Add auth

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,6 +10,12 @@ use App\Project;
 class DashboardController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    
     public function index()
     {
         
