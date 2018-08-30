6e4fb76148bbda0919efda1b6cf8e01ccbe2b321
degangh <degang.huang@gmail.com>
2018-08-30 16:42:01 +0930

    Change default side menu to collapsed

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -33,7 +33,7 @@
 
   </head>
 
-  <body id="page-top" >
+  <body id="page-top" class='sidebar-toggled'>
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
@@ -88,7 +88,7 @@
     <div id="wrapper">
 
       <!-- Sidebar -->
-      <ul class="sidebar navbar-nav">
+      <ul class="sidebar navbar-nav toggled">
         <li class="nav-item">
           <a class="nav-link" href="/dashboard">
             <i class="fas fa-fw fa-tachometer-alt"></i>
