822dc129dbf9a1da520174c7ac35502ab7b25795
nicolas <degang.huang@gmail.com>
2018-09-04 22:20:44 +0930

    Add translation files

---
+++ resources/lang/en/project.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Project Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during layout for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'new_project' => 'Add New Project',
+    'project_list' => 'Project List',
+    'project_summary' => 'Project Summary',
+    'add_member' => 'Add Members',
+    'add_task' => 'Add New Task',
+    'add_file' => 'Add Files',
+    'task_list' => 'Tasks',
+    'file' => 'Project Files',
+    'members' => 'Project Members',
+
+
+];
---
+++ resources/lang/zh-CN/project.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Project Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during layout for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'new_project' => '创建项目',
+    'project_list' => '项目列表',
+    'project_summary' => '项目概况',
+    'add_member' => '加入成员',
+    'add_task' => '创建任务',
+    'add_file' => '添加文件',
+    'task_list' => '任务列表',
+    'file' => '项目文件',
+    'members' => '项目成员',
+
+
+];
