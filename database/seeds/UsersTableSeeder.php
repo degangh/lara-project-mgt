c5057beb24d4612be835fe8409a9d8c7789d7aa9
nicolas <degang.huang@gmail.com>
2018-10-04 22:04:42 +0930

    create and modify seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,9 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'degang',
             'email' => 'hdg@sina.com',
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
+            'is_admin' => 1,
+            'locale' => 'zh-CN'
         ]);
 
         DB::table('users')->insert([
