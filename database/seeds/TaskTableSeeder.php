fa7b10d6a62fa2287a2f08ba73a44cfe9a3b87bf
nicolas <degang.huang@gmail.com>
2018-10-05 22:12:09 +0930

    add data seeding to initialize sample data

--- database/seeds/TaskTableSeeder.php
+++ database/seeds/TaskTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class TaskTableSeeder extends Seeder
 {
     /**
@@ -13,7 +14,13 @@ class TaskTableSeeder extends Seeder
     {
         //
         DB::table("tasks")->insert([
-            
+            'project_id' => 1, 
+            'name' => 'Task as an example',
+            'user_id' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'due_time' => date('Y-m-d', strtotime('+5 day', time())),
+            'assignee' => 2
         ]);
     }
 }
