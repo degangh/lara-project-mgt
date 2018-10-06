0c58556f5131138618c870e3bfec325bb42e39d0
nicolas <degang.huang@gmail.com>
2018-10-06 22:38:40 +0930

    Add member to initial data

---
+++ database/seeds/MemberTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MemberTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('project_user')->insert([
+            'project_id' => 1,
+            'user_id' => 2,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
