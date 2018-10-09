2a35852b6c3c1778ae1094a37baaf04262f414ad
nicolas <degang.huang@gmail.com>
2018-10-09 23:39:52 +1030

    create notification model

---
+++ app/Notification.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    protected $fillable = ['content', 'sender_id', 'reader_id'];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function reader()
+    {
+        return $this->belongsTo(User::class, 'reader_id');
+    }
+}
