33b37e02b3da9d5be8c1c4aef7616cb272aedf3d
nicolas <degang.huang@gmail.com>
2018-11-11 22:45:42 +1030

    implement file upload test case

--- tests/Feature/ProjectFileUploadTest.php
+++ tests/Feature/ProjectFileUploadTest.php
@@ -5,6 +5,11 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use App\Project;
+use App\Task;
+use App\User;
 
 class ProjectFileUploadTest extends TestCase
 {
@@ -15,7 +20,19 @@ class ProjectFileUploadTest extends TestCase
      */
     public function testOwnerUploadFile()
     {
-        
+        Storage::fake('upload');
+        $file = UploadedFile::fake()->image('test.jpg');
+        $project = Project::first();
+        $user = User::first();
+
+        $response = $this->actingAs($user)->post('/projects/'.$project->id.'/file', [
+            'attchement' => $file
+        ]);
+        $response->assertStatus(302);
+        //$response->assertSessionHas("success",__("project.file_success"));
+
+        Storage::disk('upload')->assertExists($file->hashName());
+
     }
 
     public function testMemberUploadFile()
@@ -25,6 +42,6 @@ class ProjectFileUploadTest extends TestCase
 
     public function testOutsiderUploadFile()
     {
-        
+
     }
 }
